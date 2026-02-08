c3ebfb7aebec2ae144b775e97fa8268356776f7b
Ian Lamb <ian.lamb@airbnb.com>
2026-02-08 13:15:21 -0800

    update instruments and add footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,23 @@
-import React from "react"
+import React from 'react'
 
 export const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <main>
-      {children}
-    </main>
+    <>
+      <main>{children}</main>
+      <footer>
+        Website by{' '}
+        <a href="https://ianlamb.com" target="_blank">
+          Ian Lamb
+        </a>{' '}
+        &mdash; Music icons by{' '}
+        <a
+          href="https://www.flaticon.com/free-icons/music"
+          target="_blank"
+          title="asset attribution"
+        >
+          Freepik
+        </a>
+      </footer>
+    </>
   )
 }
